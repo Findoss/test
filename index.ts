60932cb64470f967de43c67299bc679a5c411644
Nikita Stroganov <findoss@yandex.ru>
2021-01-14 13:18:32 +0300

    add log

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import template from './vendor/template.js';
 
 var compiled = template('<% _.forEach(users, function(user) { %><li><%- user %></li><% }); %>');
-compiled({ users: ['fred', 'barney'] });
+console.log(compiled({ users: ['fred', 'barney'] }));
