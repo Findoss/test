9b26345374b34c448b51a6320be42b080fc627bf
Nikita Stroganov <findoss@yandex.ru>
2021-01-14 13:30:40 +0300

    fix

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import template from './vendor/template.js';
+// import template from './vendor/template.js';
+import template from 'lodash/template';
 
 var compiled = template('<% _.forEach(users, function(user) { %><li><%- user %></li><% }); %>');
 console.log(compiled({ users: ['fred', 'barney'] }));
